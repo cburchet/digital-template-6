9dac4f02360264b2523941dfbc395856545405d1
cburchet <cburchet@gmu.edu>
2015-03-18 00:07:18 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -28,6 +28,8 @@ window.onload = function() {
     var backgroundLayer;
     var blockedLayer;
     
+    var level = 1;
+    
     function create() 
     {
         game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -61,29 +63,12 @@ window.onload = function() {
     {
     	game.physics.arcade.collide(player, blockedLayer);
     	
-         player.body.velocity.x = 0;
 	 
-    	if (cursors.left.isDown)
-    	{
-    		player.body.velocity.x = -150;
-     
-    		player.animations.play('left');
-    	}
-    	else if (cursors.right.isDown)
-    	{
-    		player.body.velocity.x = 150;
-     		player.animations.play('right');
-    	}
-    	else
-    	{
-    		player.animations.stop();
-    	
-    		player.frame = 4;
-    	}
-    	
+    	player.body.velocity.x = 150 + level * 10;
+     	player.animations.play('right');
     	if (cursors.up.isDown && player.body.onFloor())
     	{
-    		player.body.velocity.y = -350;
+    		player.body.velocity.y = -100;
     	}
     }
 };
