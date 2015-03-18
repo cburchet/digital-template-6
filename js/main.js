25574128db9b0bd9f7354eb7bf0b81f6644886c7
cburchet <cburchet@gmu.edu>
2015-03-17 23:53:08 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,7 +19,9 @@ window.onload = function() {
         // Load an image and call it 'logo'.
         game.load.tilemap('world', 'assets/world.json', null, Phaser.Tilemap.TILED_JSON);
         game.load.image( 'gameTiles', 'assets/tiles.png' );
+        game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     }
+    var player;
     
     var map;
     var backgroundLayer;
@@ -39,10 +41,44 @@ window.onload = function() {
         
         backgroundLayer = map.createLayer('BackgroundLayer');
         backgroundLayer.resizeWorld();
+        
+        player = game.add.sprite(100, game.world.height - 150, 'dude');
+	 
+    	game.physics.enable(player);
+	    game.camera.follow(player);
+	
+	    player.body.bounce.y = 0.2;
+	    player.body.gravity.y = 300;
+		
+	    player.animations.add('left', [0, 1, 2, 3], 10, true);
+	    player.animations.add('right', [5, 6, 7, 8], 10, true);
     }
     
     function update() 
     {
-
+         player.body.velocity.x = 0;
+	 
+    	if (cursors.left.isDown)
+    	{
+    		player.body.velocity.x = -150;
+     
+    		player.animations.play('left');
+    	}
+    	else if (cursors.right.isDown)
+    	{
+    		player.body.velocity.x = 150;
+     		player.animations.play('right');
+    	}
+    	else
+    	{
+    		player.animations.stop();
+    	
+    		player.frame = 4;
+    	}
+    	
+    	if (cursors.up.isDown && player.body.onFloor())
+    	{
+    		player.body.velocity.y = -350;
+    	}
     }
 };
