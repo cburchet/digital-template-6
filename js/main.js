b3ca6d339d89173376516fbf338f65af194a6c07
cburchet <cburchet@gmu.edu>
2015-03-18 23:16:47 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -118,7 +118,7 @@ window.onload = function() {
     
     function collectPhones()
     {
-    	phone.destroy();
+    	phones.destroy();
     	score++;
     	var scoreText = game.add.text(0, 0, 'Score: ' + score, { fontSize: '128px', fill: 'red' });
     	scoreText.fixedToCamera = true;
